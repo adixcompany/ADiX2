db0546d3d5fed794b3359e7d8529ce7994f0b3c8
AD Company <arashranjer026@gmail.com>
2017-01-03 17:21:58 +0330

    Update delmsg.lua

--- plugins/delmsg.lua
+++ plugins/delmsg.lua
@@ -3,23 +3,23 @@
     delete_msg(result[i].id, ok_cb, false)
   end
   if tonumber(extra.con) == #result then
-    send_msg(extra.chatid, '"'..#result..'" Recentry messages has been removed !', ok_cb, false)
+    send_msg(extra.chatid, '"'..#result..'" تمام!', ok_cb, false)
   else
-    send_msg(extra.chatid, 'Mentioned messages has been removed !', ok_cb, false)
+    send_msg(extra.chatid, 'تمام!', ok_cb, false)
   end
 end
 local function run(msg, matches)
   if matches[1] == 'del' and is_momod2(msg) then
     if msg.to.type == 'channel' then
       if tonumber(matches[2]) > 10000000000 or tonumber(matches[2]) < 1 then
-        return "Please choose more than 1"
+        return "لطفا بیشتر از 1 پیام را انتخاب کنید"
       end
       get_history(msg.to.peer_id, matches[2] + 1 , history , {chatid = msg.to.peer_id, con = matches[2]})
     else
-      return "Just sopergroup"
+      return "فقط در سوپرگروه ها"
     end
   else
-    return "You can not clean msgs"
+    return "Error!You are not a allowed user.Please see bot catalog with /help !Error code: XM1"
   end
 end
 
