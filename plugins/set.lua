e3a7c4dbd842facc76eebea58120dcb2b3f47d6e
AD Company <arashranjer026@gmail.com>
2017-01-03 19:37:47 +0330

    Update set.lua

--- plugins/set.lua
+++ plugins/set.lua
@@ -1,6 +1,6 @@
 local function save_value(msg, name, value)
   if (not name or not value) then
-    return "Usage: !set var_name value"
+    return "روش استفاده از فضای ابری: !set var_name value"
   end
   local hash = nil
   if msg.to.type == 'chat' or msg.to.type == 'channel'  then
@@ -8,12 +8,12 @@ local function save_value(msg, name, value)
   end
   if hash then
     redis:hset(hash, name, value)
-    return "Saved "..name
+    return "در فضای ابری ذخیره شد "..name
   end
 end
 local function run(msg, matches)
   if not is_momod(msg) then
-    return "For moderators only!"
+    return "Error!You are not a allowed user.Please see bot catalog with /help !Error code: XM1"
   end
   local name = string.sub(matches[1], 1, 50)
   local value = string.sub(matches[2], 1, 1000)
