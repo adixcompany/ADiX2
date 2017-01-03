90f1c416b3bc59a8ee257cd922e70ed9d08bccbd
AD Company <arashranjer026@gmail.com>
2017-01-03 19:35:42 +0330

    Update invite.lua

--- plugins/invite.lua
+++ plugins/invite.lua
@@ -5,11 +5,11 @@ local function callbackres(extra, success, result)
 	local chat = 'chat#id'..extra.chatid
 	local channel = 'channel#id'..extra.chatid
 	if is_banned(result.id, extra.chatid) then 
-        send_large_msg(chat, 'User is banned.')
-        send_large_msg(channel, 'User is banned.')
+        send_large_msg(chat, 'کاربر بن هست!برای خارج کردن او کاتالوگ را با دستور /help بخوانید')
+        send_large_msg(channel, 'کاربر بن هست!برای خارج کردن او کاتالوگ را با دستور /help بخوانید')
 	elseif is_gbanned(result.id) then
-	    send_large_msg(chat, 'User is globaly banned.')
-		send_large_msg(channel, 'User is globaly banned.')
+	    send_large_msg(chat, 'کاربر بن هست!برای خارج کردن او کاتالوگ را با دستور /help بخوانید(Globaly Mode)')
+		send_large_msg(channel, 'کاربر بن هست!برای خارج کردن او کاتالوگ را با دستور /help بخوانید(Globaly Mode)')
 	else    
 	    chat_add_user(chat, user, ok_cb, false) 
 		channel_invite(channel, user, ok_cb, false)
@@ -21,11 +21,11 @@ function run(msg, matches)
 	return
   end
   if not is_admin1(msg) then -- For admins only !
-		return 'Only admins can invite.'
+		return 'Error!You are not a allowed user.Please see bot catalog with /help !Error code: XM1'
   end
   if not is_realm(msg) then
     if data[tostring(msg.to.id)]['settings']['lock_member'] == 'yes' and not is_admin1(msg) then
-		  return 'Group is private.'
+		  return 'گروه شخصی است'
     end
   end
 	if msg.to.type ~= 'chat' or msg.to.type ~= 'channel' then 
